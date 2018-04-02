49354d3af189d100d3eda85d672c5e183ae8a924
Daniel Kerrigan <dkerriga@nd.edu>
2018-04-02 10:05:27 -0400

    press button on phone to post to server

---
+++ rpi/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def command():
+    if request.method == 'POST':
+        print(request.get_json())
+        return jsonify(success=True)
+
+    return jsonify(success=False)
