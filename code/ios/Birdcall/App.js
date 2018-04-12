b2acd6ee1f08927e022f5048eee8ffbe979e0854
Daniel Kerrigan <dkerriga@nd.edu>
2018-04-12 01:52:53 -0400

    added icons, implemented connect, started waypoints

--- code/ios/Birdcall/App.js
+++ code/ios/Birdcall/App.js
@@ -8,9 +8,13 @@ import {
   Text,
   View,
   Alert,
+  AlertIOS,
+  Button,
+  FlatList,
   TouchableHighlight
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Voice from 'react-native-voice';
 
 export class VoiceButton extends Component {
@@ -62,7 +66,7 @@ export class VoiceButton extends Component {
   recordingPressed(e) {
     if (this.state.isRecording) {
       this.stopRecording(e);
-      fetch('http://127.0.0.1:5000/', {
+      fetch(this.props.connectionUrl, {
           method: 'POST',
           headers: {
               'content-type': 'application/json'
@@ -86,9 +90,11 @@ export class VoiceButton extends Component {
   }
 
   render() {
+    let iconName = this.state.isRecording ? 'stop' : 'microphone';
+    let iconColor = this.state.isRecording ? 'red' : 'black';
     return (
-      <TouchableHighlight onPress={this.recordingPressed.bind(this)}>
-        <Text>{this.state.isRecording ? 'Stop' : 'Start'}</Text>
+      <TouchableHighlight onPress={this.recordingPressed.bind(this)} style={styles.voice}>
+        <Icon name={iconName} size={96} color={iconColor}/>
       </TouchableHighlight>
     );
   }
@@ -96,30 +102,89 @@ export class VoiceButton extends Component {
 
 
 export default class App extends Component<Props> {
+  connect
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+        connectionUrl: 'http://127.0.0.1:5000',
+        waypoints: [{key: 'test'}, {key: 'test2'}, {key: 'testtesttesttest'}, {key: 'test4'}, {key: 'test5'}, {key: 'test6'}]
+    };
+  }
+
+  connectDronePressed(e) {
+    AlertIOS.prompt(
+      'Enter Raspberry Pi URL',
+      null,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancelled Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'Connect',
+          onPress: (connection) => this.setState({'connectionUrl': connection}),
+        },
+      ],
+      'plain-text'
+    );
+  }
+
+  addWaypointPressed(e) {
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <VoiceButton/>
+        <Text style={styles.header}>Birdcall</Text>
+        <VoiceButton connectionUrl={this.state.connectionUrl}/>
+        <Text style={styles.smallerHeader}>Waypoints</Text>
+        <View style={styles.waypointContainer}>
+            <FlatList
+              data={this.state.waypoints}
+              renderItem={({item, index}) => <Text style={styles.item}>{index + 1}. {item.key}</Text>}
+              ItemSeparatorComponent={() => <View style={{height: 1, width: '100%', backgroundColor:'#dddddd', margin: 5}}/>}/>
+        </View>
+        <Button title='Add Waypoint' onPress={this.addWaypointPressed.bind(this)}/>
+        <Button title='Connect to Drone' onPress={this.connectDronePressed.bind(this)}/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  voice: {
+      margin: 10,
+      borderWidth: 2,
+      borderColor: 'black',
+      borderRadius: 5,
+      width: 150,
+      height: 150,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  waypointContainer: {
     margin: 10,
+    height: 155,
+    backgroundColor: '#eeeeee',
   },
-  instructions: {
+  item: {
+    textAlign: 'left',
+    fontSize: 18,
+  },
+  header: {
+    fontSize: 48,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  smallerHeader: {
+    fontSize: 24,
+    textAlign: 'center',
   },
 });
